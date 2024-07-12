7027b54591e9c3881eb482ffdb8c4ce8e871d8e6
ManasaGitClass <kmanasa82@gmail.com>
2024-07-12 10:33:14 +0530

    Update app2.js

--- app2.js
+++ app2.js
@@ -3,3 +3,5 @@ const greet = require('./app1');
 // Use the function from the module
 const message = greet('World');
 console.log(message);
+console.log("working with git")
+console.log("DS-A class")
